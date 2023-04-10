5628b17382d0648beba209eb9eb69aa12dd1541b
Francois Robbertze <francoisdprob@gmail.com>
2023-04-10 22:56:22 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Log In</h1>
+    <form action="/php/database.php" method="post">
+        <!-- email -->
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email"></br>
+        <!-- password -->
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password"></br>
+        <input type="submit" value="Login">
+</body>
+</html>
---
+++ php/database.php
@@ -0,0 +1,60 @@
+<?php
+require_once '../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $db = new Database();
+    $db->validate($email, $password);
+    $db->closedb();
+}
+
+class Database {
+    public $host = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $database = "gym";
+    public $conn;
+
+    function connectdb () {
+        $this->password = $_ENV["MYSQLPASS"];
+        $mysqli = new mysqli($this->host, $this->username, $this->password, $this->database);
+        if ($mysqli->connect_errno) {
+            return False;
+        }
+        else {
+            $this->conn = $mysqli;
+            return True;
+        }
+    }
+
+    function closedb () {
+        mysqli_close($this->conn);
+    }
+
+    function validate ($email, $password) {
+        if ($this->connectdb()) {
+            $sql = "SELECT * FROM members WHERE email = '$email' AND password = '$password'";
+            $result = $this->conn->query($sql);
+            if ($result->num_rows > 0) {
+                $data = $result->fetch_assoc();
+                $name = $data['first_name'];
+                $surname = $data['last_name'];
+                echo "<h1>Welcome $name $surname</h1>";
+            }
+            else {
+                echo "Login Failed";
+            }
+        }
+        else {
+            echo "Connection Failed";
+        }
+    }
+
+
+}
+?>
