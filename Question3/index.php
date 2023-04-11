c2aa9bd7a333a2397714287b04812dabf43d9bca
Francois Robbertze <francoisdprob@gmail.com>
2023-04-11 15:54:11 +0200

    Question 3

---
+++ Question3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <?php require_once __DIR__ . '/vendor/autoload.php'; ?>
+</head>
+<body>
+<div
+style="block-size: 100vh; display: flex; justify-content: center; align-items: center;">
+<form action="php/mail.php" method="post">
+  <h1>Register</h1>
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required><br>
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required><br>
+  <label for="age">Age:</label>
+  <input type="number" id="age" name="age" required><br>
+  <label for="address">Address:</label>
+  <input type="text" id="address" name="address" required><br>
+  <label for="gender">Gender:</label>
+  <select id="gender" name="gender">
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+    <option value="non-binary">Non-Binary</option>
+  </select><br>
+  <label for="favorite_character">Favorite Comic Book Character:</label><br>
+  <input type="text" id="favorite_character" name="favorite_character"><br>
+  <button type="submit">Register</button>
+</form>
+</div>
+
+</body>
+</html>
---
+++ Question3/php/survey.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kimiko Entertainment Survey</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+            line-height: 1.5;
+            margin: 0;
+            padding: 0;
+            background-color: #800000;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 10vh auto;
+            padding: 2vw;
+            background-color: #ffffff;
+            border-radius: 2vw;
+            box-shadow: 0 0 1vh #000000;
+            text-align: center;
+        }
+
+        h1 {
+            margin-top: 0;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+            font-size: 18px;
+            font-weight: bold;
+        }
+
+        input[type=text], input[type=number], textarea {
+            width: 80%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            font-size: 16px;
+            resize: vertical;
+        }
+
+        button {
+            background-color: #800000;
+            color: #ffffff;
+            border: none;
+            border-radius: 5px;
+            padding: 1vw;
+            font-size: 1vw;
+            font-weight: bold;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        button[type=submit]:hover {
+            background-color: #9b2c2c;
+        }
+
+        .error-message {
+            color: red;
+            font-size: 14px;
+            margin-top: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Kimiko Entertainment Survey</h1>
+        <p>We would like to know what you think about Kimiko Entertainment!</p>
+        <form id="survey-form" method="post">
+            <label for="hero">Who would you like to see at the next Comicon?</label>
+            <input type="text" id="hero" name="hero" required>
+            <label for="rating">How would you rate our service?</label>
+            <input type="number" id="rating" name="rating" min="1" max="10" required>
+            <label for="comment">Any comments?</label>
+            <textarea id="comment" name="comment" rows="5"></textarea><br>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
